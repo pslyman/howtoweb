fe4dda547d1269244906ad241348a1fbaece89aa
pslyman <dev@porterlyman.com>
2022-03-26 17:48:41 -0600

    initial commit with boneworks

---
+++ src/components/PostItem.tsx
@@ -0,0 +1,36 @@
+import { PostContent } from "../lib/posts";
+import Date from "./Date";
+import Link from "next/link";
+import { parseISO } from "date-fns";
+
+type Props = {
+  post: PostContent;
+};
+export default function PostItem({ post }: Props) {
+  return (
+    <Link href={"/posts/" + post.slug}>
+      <a>
+        <Date date={parseISO(post.date)} />
+        <h2>{post.title}</h2>
+        <p>{post.description}</p>
+        <style jsx>
+          {`
+            a {
+              color: #222;
+              display: inline-block;
+            }
+            h2 {
+              margin: 0;
+              font-weight: 500;
+            }
+            p {
+              margin-top: 0;
+              font-style: italic;
+              opacity: 70%;
+            }
+          `}
+        </style>
+      </a>
+    </Link>
+  );
+}
